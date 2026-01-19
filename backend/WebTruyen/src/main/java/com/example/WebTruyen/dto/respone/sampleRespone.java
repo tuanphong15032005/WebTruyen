f42701ef80e83559520d31fe1d66547676ef4dac
Phong <phanimeong@gmail.com>
2026-01-19 17:16:45 +0700

    update cau truc

---
+++ backend/WebTruyen/src/main/java/com/example/WebTruyen/dto/respone/sampleRespone.java
@@ -0,0 +1,14 @@
+package com.example.WebTruyen.dto.respone;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Phong
+ */
+public class sampleRespone {
+    
+}
