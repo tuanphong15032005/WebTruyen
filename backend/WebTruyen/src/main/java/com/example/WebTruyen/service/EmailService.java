f6e6020799e96f235bb4339e069f3eea675e73d7
Hieu Son <nguyenhieuson69@gmail.com>
2026-02-03 00:27:21 +0700

    "Comment error code - Login"

--- backend/WebTruyen/src/main/java/com/example/WebTruyen/service/EmailService.java
+++ backend/WebTruyen/src/main/java/com/example/WebTruyen/service/EmailService.java
@@ -1,23 +1,23 @@
-package com.example.WebTruyen.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailService {
-
-    @Autowired
-    private JavaMailSender mailSender;
-
-    public void sendVerificationEmail(String toEmail, String code) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("WebTruyen <noreply@webtruyen.com>");
-        message.setTo(toEmail);
-        message.setSubject("Mã xác thực đăng ký WebTruyen");
-        message.setText("Xin chào,\n\nMã xác thực của bạn là: " + code + "\n\nVui lòng không chia sẻ mã này cho ai.");
-        
-        mailSender.send(message);
-    }
-}
+//package com.example.WebTruyen.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class EmailService {
+//
+//    @Autowired
+//    private JavaMailSender mailSender;
+//
+//    public void sendVerificationEmail(String toEmail, String code) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom("WebTruyen <noreply@webtruyen.com>");
+//        message.setTo(toEmail);
+//        message.setSubject("Mã xác thực đăng ký WebTruyen");
+//        message.setText("Xin chào,\n\nMã xác thực của bạn là: " + code + "\n\nVui lòng không chia sẻ mã này cho ai.");
+//
+//        mailSender.send(message);
+//    }
+//}
