01455831f2b78f23f9e195a2dfc2a503f4594986
Phong <phanimeong@gmail.com>
2026-02-19 22:18:45 +0700

    chore(frontend): setup tailwind and base project config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom' // 1. Import cái này
+import { WalletProvider } from './context/WalletContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <WalletProvider>
+        <App />
+      </WalletProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
