d8c34d486e0245afe2a15565cfae851c930e9d7a
Hieu Son <nguyenhieuson69@gmail.com>
2026-03-20 01:21:20 +0700

    thêm kind cmt cho notify

--- backend/WebTruyen/src/main/java/com/example/WebTruyen/entity/enums/NotificationKind.java
+++ backend/WebTruyen/src/main/java/com/example/WebTruyen/entity/enums/NotificationKind.java
@@ -1,3 +1,9 @@
 package com.example.WebTruyen.entity.enums;
 
-public enum NotificationKind {new_chapter, topup, report, system}
+public enum NotificationKind {
+    new_chapter,
+    topup,
+    report,
+    system,
+    comment
+}
